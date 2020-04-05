1ab7c927082507ed7d51d9937f1ea8757329e9d2
mykelg <bt.camino28@gmail.com>
2020-04-05 16:43:26 +0900

    enable VA != PA
    PA: data & bss are located rom on code
    VA: data & bss are located ram on execution

--- os/main.c
+++ os/main.c
@@ -1,6 +1,10 @@
 #include "lib.h"
 
 static void init(void) {
+  extern int rodata_end, data_start, data_end, bss_start, bss_end;
+  memcpy(&data_start, &rodata_end, (size_t)(&data_end) - (size_t)(&data_start));
+  memset(&bss_start, 0, (size_t)(&bss_end) - (size_t)(&bss_start));
+
   serial_init();
 }
 

5fdcf3690849102bd0982c8e64a902e80b669514
mykelg <bt.camino28@gmail.com>
2020-04-26 02:00:55 +0900

    Remove embedded binary from micos

--- os/test.c
+++ os/test.c
@@ -25,15 +25,12 @@ static void test_memory_section(void) {
   puts("[TEST] section address test\n");
   extern int text_start, text_end;
   extern int rodata_start, rodata_end;
-  extern int osdata_start, osdata_end;
   extern int data_start, data_end;
   extern int bss_start, bss_end;
   puts(" .text  : "); putxvald(&text_start,8); puts("-"); putxvald(&text_end,8);
   puts(" (len:"); putxval((int)(&text_end) - (int)(&text_start)); puts(")\n");
   puts(" .rodata: "); putxvald(&rodata_start,8); puts("-"); putxvald(&rodata_end,8);
   puts(" (len:"); putxval((int)(&rodata_end) - (int)(&rodata_start)); puts(")\n");
-  puts(" .osdata: "); putxvald(&osdata_start,8); puts("-"); putxvald(&osdata_end,8);
-  puts(" (len:"); putxval((int)(&osdata_end) - (int)(&osdata_start)); puts(")\n");
   puts(" .data  : "); putxvald(&data_start,8); puts("-"); putxvald(&data_end,8);
   puts(" (len:"); putxval((int)(&data_end) - (int)(&data_start)); puts(")\n");
   puts(" .bss   : "); putxvald(&bss_start,8); puts("-"); putxvald(&bss_end,8);
@@ -80,16 +77,6 @@ static void test_memory_dump() {
   puts("\n");
 }
 
-static void test_memory_dump_osdata() {
-  puts("[TEST] Memory dump osdata\n");
-  extern int osdata_start, osdata_end;
-  const char* osdata = &osdata_start;
-  puts(" ");
-  puts(osdata);
-  dump(osdata, (size_t)(&osdata_end) - (size_t)(&osdata_start));
-  puts("\n");
-}
-
 void test(void) {
   test_ram_init();
   test_memory_mapping();
@@ -97,5 +84,4 @@ void test(void) {
   test_memcpy();
   test_memset();
   test_memory_dump();
-  test_memory_dump_osdata();
 }
